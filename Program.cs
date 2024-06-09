87ef494712cbb86a641c97b93bafd381b183852b
yoona0805 <145645490+yoona0805@users.noreply.github.com>
2024-06-09 22:22:41 +0900

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Greeting, Yoona!");
 
-int sum = 0;
-for (int i =0 ; i < 10 l i++)
-  Console.WriteLine("{0}*{1} = {2}", sum,i,sum*i);
+for (int j =2; j < 10; j++)
+  for (int i =1 ; i < 10 ; i++)
+    Console.WriteLine("{0}*{1} = {2}", j,i,j*i);
